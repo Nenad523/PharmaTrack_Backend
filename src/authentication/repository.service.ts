d2ca5bb8774336bbad0430f8b8d1d3e312c529b5
RoninDev <npopo4321@gmail.com>
2026-04-12 10:35:38 +0200

    Added connection to database

--- src/authentication/repository.service.ts
+++ src/authentication/repository.service.ts
@@ -2,10 +2,12 @@
 import { Injectable } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
-
+import { DatabaseService } from 'src/database/database.service';
 @Injectable()
 export class RepositoryService {
 
+    constructor(private db: DatabaseService) {};
+    
     login(loginInfo: LoginDto){
         return loginInfo;
     }
