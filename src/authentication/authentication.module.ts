c47584d9e57dc9af513fb5a1bcad72d7bcaab30d
RoninDev <npopo4321@gmail.com>
2026-04-18 14:16:23 +0200

    Started development on Email validation

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -6,10 +6,11 @@ import { RepositoryService } from './repository.service';
 import { LocalStrategy } from './local-strategy.service';
 import { AuthenticationController } from './authentication.controller';
 import { SessionSerializer } from './session.serializer';
+import { EmailService } from './email.service';
 
 @Module({
   imports: [PassportModule.register({ session: true })],
   controllers: [AuthenticationController],
-  providers: [AuthenticationService, RepositoryService, LocalStrategy, SessionSerializer]
+  providers: [AuthenticationService, RepositoryService, LocalStrategy, SessionSerializer, EmailService]
 })
 export class AuthenticationModule {}
