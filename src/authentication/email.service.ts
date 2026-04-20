a72c67e39e333bf2af85bf5cc135a4d1becbc2e5
RoninDev <npopo4321@gmail.com>
2026-04-20 17:19:40 +0200

    Email2

--- src/authentication/email.service.ts
+++ src/authentication/email.service.ts
@@ -6,9 +6,7 @@ import * as nodemailer from 'nodemailer'
 export class EmailService {
     
   private transporter = nodemailer.createTransport({
-      service: 'smtp.gmail.com',
-      port: 587,        // explicit is better
-      secure: false,
+      service: 'gmail',
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS  // App password, ne obična lozinka
