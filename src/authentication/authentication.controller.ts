77cce1bd3dc4e2991d9032c64de2defea2d58b63
RoninDev <npopo4321@gmail.com>
2026-04-26 19:28:53 +0200

    Add swagger summary for all endpoints

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -9,12 +9,14 @@ import { AuthenticationService } from './authentication.service';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('api/v1/auth')
 export class AuthenticationController {
 
     constructor(private authService: AuthenticationService) {}
 
+    @ApiOperation({ 'summary' : 'Prijavljivanje putem emaila i lozinke'})
     @UseGuards(AuthGuard('local'))
     @Post('/login')
     async login(@Body() _loginDto: LoginDto, @Request() req){
@@ -32,17 +34,20 @@ export class AuthenticationController {
         return req.user;
     }
 
+    @ApiOperation({ 'summary' : 'Dohvatanje podataka o trenutno prijavljenom korisniku.'})
     @Get('/me')
     getMe(@Request() req){
         return req.user;
     }
 
+    @ApiOperation({ 'summary' : 'Odjavljivanje sa prijavljenog naloga.'})
     @Post('/logout')
     logout(@Request() req){
         req.logout(() => {});
         return { message : 'Logged out'};
     }
 
+    @ApiOperation({ 'summary' : 'Kreiranje naloga.'})
     @Post('/register')
     async register(@Body() body: RegisterDto) {
         const { email, password, fullName } = body;
@@ -50,6 +55,7 @@ export class AuthenticationController {
         return this.authService.register(email, password, fullName);
     }
 
+    @ApiOperation({ 'summary' : 'Verifikacija mail adrese nakon kreiranja naloga.'})
     @Get('/verify-email')
     async verifyEmail(@Query('token') token: string){
         return this.authService.verifyEmail(token);
--- src/medication/medication.controller.ts
+++ src/medication/medication.controller.ts
@@ -5,29 +5,33 @@ import { Controller, Get, Query, Param } from '@nestjs/common';
 import { MedicationDto } from './dto/medication-dto';
 import { MedicationService } from './medication.service';
 import { ParsePositiveIntPipe } from '../common/pipes/parse-positive-int.pipe';
-import { ApiQuery } from '@nestjs/swagger';
+import { ApiOperation, ApiQuery } from '@nestjs/swagger';
 
 @Controller('api/v1/medication')
 export class MedicationController {
 
     constructor(private medicationService: MedicationService) {}
 
+    @ApiOperation({ 'summary' : 'Pretraga lijekova po nazivu.'})
     @Get('/search')
     @ApiQuery({ name: 'name', required: false, type: String })
     searchMedications(@Query() medication: MedicationDto){
         return this.medicationService.searchAll(medication.name);
     }
 
+    @ApiOperation({ 'summary' : 'Dohvatanje detaljnih informacija o konkretnom lijeku.'})
     @Get(':id')
     searchMedication(@Param('id', ParsePositiveIntPipe) id: number){
         return this.medicationService.searchOne(id);
     }
 
+    @ApiOperation({ 'summary' : 'Dohvatanje svih aktivnih doza za odabrani lijek.'})
     @Get(':id/doses')
     getDoses(@Param('id', ParsePositiveIntPipe) id: number){
         return this.medicationService.searchDoses(id);
     }
 
+    @ApiOperation({ 'summary' : 'Dohvatanje alternativnih lijekova koji dijele istu aktivnu supstancu.'})
     @Get(':id/alternatives')
     getAlternatives(@Param('id', ParsePositiveIntPipe) id: number){
         return this.medicationService.getAlternatives(id);
