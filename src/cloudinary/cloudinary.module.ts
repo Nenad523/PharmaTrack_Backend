9a2d6016ac9eaf587ddc5d5a3a5abca522b808ef
RoninDev <npopo4321@gmail.com>
2026-05-05 10:24:10 +0200

    added: cloudinary controller

--- src/cloudinary/cloudinary.module.ts
+++ src/cloudinary/cloudinary.module.ts
@@ -1,9 +1,11 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { CloudinaryService } from './cloudinary.service';
+import { CloudinaryController } from './cloudinary.controller';
 
 @Module({
   providers: [CloudinaryService],
   exports: [CloudinaryService],
+  controllers: [CloudinaryController],
 })
 export class CloudinaryModule {}
