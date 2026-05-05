766259f200c316ee69ec142330fa310ca5388b00
RoninDev <npopo4321@gmail.com>
2026-05-05 10:22:20 +0200

    added: service for image upload

---
+++ src/cloudinary/cloudinary.service.ts
@@ -0,0 +1,38 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import { v2 as cloudinary } from 'cloudinary';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class CloudinaryService {
+  constructor(private config: ConfigService) {
+    cloudinary.config({
+      cloud_name: this.config.get('CLOUDINARY_CLOUD_NAME'),
+      api_key: this.config.get('CLOUDINARY_API_KEY'),
+      api_secret: this.config.get('CLOUDINARY_API_SECRET'),
+    });
+  }
+
+  async uploadImage(file: Express.Multer.File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      cloudinary.uploader.upload_stream(
+        {
+          folder: 'pharmatrack/medications',
+          transformation: [
+            { width: 500, height: 500, crop: 'limit' },  // max 500x500
+            { quality: 'auto' },                          // automatska kompresija
+            { fetch_format: 'auto' }                      // optimalni format
+          ]
+        },
+        (error, result) => {
+          if (error) reject(error);
+          else resolve(result!.secure_url);
+        }
+      ).end(file.buffer);
+    });
+  }
+
+  async deleteImage(publicId: string): Promise<void> {
+    await cloudinary.uploader.destroy(publicId);
+  }
+}
