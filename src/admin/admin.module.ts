a3857bc17df1f86ab325da4c73b589789b8a00a6
RoninDev <npopo4321@gmail.com>
2026-05-18 11:57:09 +0200

    Added skeleton directory for admin endpoints

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
-import { RepositoryController } from './repository/repository.controller';
+import { RepositoryController } from './repository.controller';
+import { RepositoryService } from './repository.service';
 
 @Module({
-  providers: [AdminService],
+  providers: [AdminService, RepositoryService],
   controllers: [AdminController, RepositoryController]
 })
 export class AdminModule {}
