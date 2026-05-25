cb36850c54aa871cda278cc744da90f076129018
RoninDev <npopo4321@gmail.com>
2026-05-25 12:25:12 +0200

    Added update-pharmacy.dto.ts

---
+++ src/admin/dto/update-pharmacy.dto.ts
@@ -0,0 +1,39 @@
+/* eslint-disable prettier/prettier */
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString, MinLength, MaxLength, IsNumber, Min, Max } from 'class-validator';
+
+export class UpdatePharmacyDto {
+    
+  @ApiProperty({ example: 'Apoteka Zdravlje' })
+  @IsOptional()
+  @IsString()
+  @MinLength(3)
+  @MaxLength(100)
+  name?: string;
+
+  @ApiProperty({ example: 'Bulevar Svetog Petra Cetinjskog 12, Podgorica' })
+  @IsOptional()
+  @IsString()
+  @MinLength(5)
+  @MaxLength(200)
+  address?: string;
+
+  @ApiProperty({ example: 42.441286 })
+  @IsOptional()
+  @IsNumber()
+  @Min(-90)
+  @Max(90)
+  latitude?: number;
+
+  @ApiProperty({ example: 19.262892 })
+  @IsOptional()
+  @IsNumber()
+  @Min(-180)
+  @Max(180)
+  longitude?: number;
+
+  @ApiProperty({ example: 1 })
+  @IsOptional()
+  @IsNumber()
+  city_id?: number;
+}
