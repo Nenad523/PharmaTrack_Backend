02f4fbb2bf9d6f86631e3ebae7e8f2e16668c1e5
RoninDev <npopo4321@gmail.com>
2026-05-31 20:11:03 +0200

    Started implementation on notification service

--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { RepositoryService } from './repository.service';
 
 @Injectable()
-export class NotificationsService {}
+export class NotificationsService {
+
+    constructor(private repo: RepositoryService) {}
+
+    async subscribe(userId: number, doseId: number) {
+        return this.repo.subscribe(userId, doseId);
+    }
+
+    async unsubscribe(userId: number, notificationId: number) {
+        return this.repo.unsubscribe(userId, notificationId);
+    }
+
+    async getMyNotifications(userId: number) {
+        return this.repo.getUserNotifications(userId);
+    }
+}
