53ab3777c6b9447f9e0d4e4020f9a7d9176a8b14
RoninDev <npopo4321@gmail.com>
2026-05-31 21:03:11 +0200

    Modified notifications.module.ts

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
 import { RepositoryService } from './repository.service';
+import { EmailService } from '../authentication/email.service';
 
 @Module({
   controllers: [NotificationsController],
-  providers: [NotificationsService, RepositoryService]
+  providers: [NotificationsService, RepositoryService, EmailService],
+  exports: [NotificationsService],
 })
 export class NotificationsModule {}
