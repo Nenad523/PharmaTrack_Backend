c2fba5290be31b18c2a2c3fec30cdf8f792f523f
RoninDev <npopo4321@gmail.com>
2026-05-31 20:37:04 +0200

    Added skeleton code for repository

--- src/notifications/repository.service.ts
+++ src/notifications/repository.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { DatabaseService } from '../database/database.service';
+import { NotificationItem, NotificationSubscriber } from './types/notification.type';
 
 @Injectable()
-export class RepositoryService {}
+export class RepositoryService {
+
+    constructor(private db: DatabaseService) {}
+
+    async subscribe(userId: number, doseId: number) {}
+
+    async unsubscribe(userId: number, notificationId: number) {}
+
+    async getUserNotifications(userId: number) {}
+
+    async getSubscribersForDose(doseId: number): Promise<NotificationSubscriber[]> { return []; }
+
+    async markAsNotified(notificationIds: number[]) {}
+}
